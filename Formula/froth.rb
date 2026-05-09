f654d3696668d79d70c47ba35e5a670047da817e
Niko Kozak <nikokozak@gmail.com>
2026-05-09 11:18:48 -0400

    froth 0.1.2

--- Formula/froth.rb
+++ Formula/froth.rb
@@ -1,9 +1,8 @@
 class Froth < Formula
   desc "Live lexical language for programmable devices"
   homepage "https://frothlang.org"
-  url "https://github.com/nikokozak/froth/archive/fb618da89a5f7f65f3d5b45b671f9629b86dbd80.tar.gz"
-  version "0.1.0"
-  sha256 "53955689edff65f401b5efff9be20b3d43589ec9f0949a72b5abf385f9adef84"
+  url "https://github.com/nikokozak/froth/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "e4d671031c7356403b435e39cf7701557c52e7fed337eb9a90541a8aebab164d"
   license "MIT"
 
   head "https://github.com/nikokozak/froth.git", branch: "main"
